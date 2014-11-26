5c76ac8bda2faa29f90c1154fb2ca33a3a9cf335
Michael Yu <ItzWarty@gmail.com>
2014-11-26 11:27:26 -0800

    Add FileLock and ReferenceCounter from libwarty 87cb3967f7a01c574dd08e683dcc9d0a1c047376.

---
+++ FileLock.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace Dargon {
+   public class FileLock : ReferenceCounter<FileLock> {
+      private readonly string path;
+      private FileStream fileStream;
+
+      public FileLock(string path) { this.path = path; }
+
+      protected override void Initialize() {
+         while (true) {
+            try {
+               fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None);
+               break;
+            } catch (IOException) {
+               // file already open
+               Thread.Sleep(100);
+               continue;
+            }
+         }
+      }
+
+      protected override void Destroy() { if (fileStream != null) fileStream.Dispose(); }
+
+      protected override FileLock GetExposed() { return this; }
+   }
+}
