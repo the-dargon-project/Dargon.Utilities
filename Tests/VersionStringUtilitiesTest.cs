4ac7ced3fe4507f1e643cbf2458a3cdaa7f1e1f3
Michael Yu <ItzWarty@gmail.com>
2015-11-28 05:12:51 -0800

    VersionStringUtilities now static.

--- Tests/VersionStringUtilitiesTest.cs
+++ Tests/VersionStringUtilitiesTest.cs
@@ -5,30 +5,28 @@ namespace Dargon
 {
    public class VersionStringUtilitiesTest : NMockitoInstance
    {
-      private VersionStringUtilities testObj = new VersionStringUtilities();
-
       private const string kInvalidPath = "123ids890/A()!K/!)(DKQ:";
       private const string kValidPath = "123ids890/1.2.3.4/!)(DKQ:";
       private const string kValidPathVersionString = "1.2.3.4";
       private const uint kValidPathVersionNumber = 0x01020304U;
 
       [Fact]
-      public void GetVersionNumberHappyPathTest() { AssertEquals(kValidPathVersionNumber, testObj.GetVersionNumber(kValidPath)); }
+      public void GetVersionNumberHappyPathTest() { AssertEquals(kValidPathVersionNumber, VersionStringUtilities.GetVersionNumber(kValidPath)); }
 
       [Fact]
-      public void GetVersionNumberSadPathReturnsUint32MaxTest() { AssertEquals(uint.MaxValue, testObj.GetVersionNumber(kInvalidPath)); }
+      public void GetVersionNumberSadPathReturnsUint32MaxTest() { AssertEquals(uint.MaxValue, VersionStringUtilities.GetVersionNumber(kInvalidPath)); }
 
       [Fact]
-      public void GetVersionStringHappyPathTest() { AssertEquals(kValidPathVersionString, testObj.GetVersionString(kValidPath)); }
+      public void GetVersionStringHappyPathTest() { AssertEquals(kValidPathVersionString, VersionStringUtilities.GetVersionString(kValidPath)); }
 
       [Fact]
-      public void GetVersionStringInvalidStringReturnsEmptyStringTest() { AssertEquals("", testObj.GetVersionString(kInvalidPath)); }
+      public void GetVersionStringInvalidStringReturnsEmptyStringTest() { AssertEquals("", VersionStringUtilities.GetVersionString(kInvalidPath)); }
 
       [Fact]
       public void TryGetVersionNumberHappyPathTest()
       {
          uint versionNumber;
-         AssertTrue(testObj.TryGetVersionNumber(kValidPath, out versionNumber));
+         AssertTrue(VersionStringUtilities.TryGetVersionNumber(kValidPath, out versionNumber));
          AssertEquals(kValidPathVersionNumber, versionNumber);
       }
 
@@ -36,7 +34,7 @@ namespace Dargon
       public void TryGetVersionNumberSadPathTest()
       {
          uint versionNumber;
-         AssertFalse(testObj.TryGetVersionNumber(kInvalidPath, out versionNumber));
+         AssertFalse(VersionStringUtilities.TryGetVersionNumber(kInvalidPath, out versionNumber));
          AssertEquals(uint.MaxValue, versionNumber);
       }
    }
--- VersionStringUtilities.cs
+++ VersionStringUtilities.cs
@@ -3,8 +3,8 @@ using System;
 using System.Text.RegularExpressions;
 
 namespace Dargon {
-   public class VersionStringUtilities {
-      public string GetVersionString(uint n) {
+   public static class VersionStringUtilities {
+      public static string GetVersionString(uint n) {
          var b0 = n & 0xFF;
          var b1 = (n >> 8) & 0xFF;
          var b2 = (n >> 16) & 0xFF;
@@ -18,13 +18,13 @@ namespace Dargon {
       /// </summary>
       /// <param name="s"></param>
       /// <returns></returns>
-      public string GetVersionString(string s) {
+      public static string GetVersionString(string s) {
          var matchResult = Regex.Match(s, @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
          if (matchResult.Success) return matchResult.Value;
          else return "";
       }
 
-      public uint GetVersionNumber(string s) {
+      public static uint GetVersionNumber(string s) {
          var parts = GetVersionString(s).Split(".");
          if (parts.Length != 4)
             return uint.MaxValue;
@@ -34,7 +34,7 @@ namespace Dargon {
          return result;
       }
 
-      public bool TryGetVersionNumber(string s, out uint versionNumber) {
+      public static bool TryGetVersionNumber(string s, out uint versionNumber) {
          var match = Regex.Match(s, @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
          if (!match.Success) {
             versionNumber = uint.MaxValue;
